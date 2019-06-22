e20192aa6d7782e923a2b6664734747d16dd375b
Tracy <tracycobrien@gmail.com>
2019-06-22 10:57:30 -0400

    updating files

---
+++ src/images.js
@@ -0,0 +1,30 @@
+import brooks from './images/brooks.png';
+import charlie from './images/charlie.png';
+import cuddles from './images/cuddles.png';
+import jay from './images/jay.png';
+import jean from './images/jean.png';
+import jenny from './images/jenny.png';
+import lilly from './images/lilly.png';
+import lizzy from './images/lizzy.png';
+import max from './images/max.png';
+import mere from './images/mere.png';
+import mike from './images/mike.png';
+import sally from './images/sally.png';
+
+const images = [
+    brooks,
+    charlie,
+    cuddles,
+    jay,
+    jean,
+    jenny,
+    lilly,
+    lizzy,
+    max,
+    mere,
+    mike,
+    sally,
+
+];
+
+export default images;
